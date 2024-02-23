60ba5503cbf1ff4a788c20eb2309d033de6bea24
twinik <tobiwinik@gmail.com>
2024-02-23 01:08:04 -0300

    fixes and profile section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,6 +10,7 @@ module.exports = {
       colors: {
         primary: "var(--color-primary)",
         secondary: "var(--color-secondary)",
+        gris: "var(--color-text-gris)",
       },
       textColor: {
         default: "var(--color-text)",
